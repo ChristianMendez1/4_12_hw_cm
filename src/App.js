7e4199cbdbea9edb75d77e4c9b5ad261bbd4ffb1
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-04-13 14:44:09 -0400

    finally finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //setup
+  let [creditcard, setCreditCard] = useState(Number);
+  let [message, setMessage] = useState("");
+  const prevCreditCard = useRef('')
 
-export default App;
+
+  //Luhn Algorithm
+  function Validcard(creditcard) {
+
+  let creditCardNumber = creditcard.toString().split('').map(Number);
+
+  for(let i=creditCardNumber.length - 2; i >= 0; i = i - 2){
+
+    let iValue = creditCardNumber[i];
+
+    iValue = iValue * 2
+
+    if (iValue > 9){
+        iValue = iValue % 10 + 1;
+    }
+
+    creditCardNumber[i] = iValue;
+ }
+
+    let total = 0;
+
+    for(let i = 0; i < creditCardNumber.length; i++){
+        total += creditCardNumber[i];
+    }
+
+    return total % 10 == 0
+  }
+
+  //useeffect checks if creditcard is valid
+  useEffect(() => {
+    prevCreditCard.current = creditcard
+    if(Validcard(prevCreditCard.current) == true){
+      setMessage("Valid");
+    } else {
+      setMessage("Invalid");
+    }
+    console.log( Validcard(prevCreditCard.current) )
+  }, [creditcard]);
+
+  //html
+   return (
+    <div>
+      <form>
+        <p>Enter Credit Card Number</p>
+        <input value={creditcard} onChange={e => setCreditCard(e.target.value)}/>
+        <p> {message} </p>
+      </form>
+    </div>
+   );
+}
+export default App; 
